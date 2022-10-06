a6acabe5838afca8c5c55c9ed45d3ac0814384d3
MD AMAAN UR RAHEMAN <adoozyone@gmail.com>
2022-10-06 22:38:31 +0530

    JS-String_Numbers_Methods

---
+++ Day_05/numbers.js
@@ -0,0 +1,16 @@
+var ab="0abcd100";
+// console.log(ab.toString())
+console.log(parseInt(ab))
+
+var mn=100.01
+console.log(parseFloat(mn))
+
+var total=110000000
+// console.log(total.toFixed(2))
+// console.log(total.toFixed(3))
+// console.log(total.toPrecision(3))
+console.log(total.toExponential())
+``
+
+
+
---
+++ Day_05/scopes.js
@@ -0,0 +1,39 @@
+var fname="john";
+let lname="wick";
+var city = "Nanded";
+// console.log(city);
+
+function yes(){
+    var city="mh";
+}
+yes ();
+
+// function getname(){
+//     var total = 100;
+//     console.log(total);
+// }
+// getname();
+// {
+//     // const age=10;
+//     // let age =10;
+//     var age=22;
+// }
+// console.log(age);
+
+
+
+// reassigning the variables 
+
+var one = 1;
+one = 2;
+// console.log(one)
+
+
+let second=3;
+second=2;
+console.log(second)
+
+
+const three=25;
+three=2;
+console.log(three)
